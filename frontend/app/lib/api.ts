3ed052a34d219fcf1edd0306f5dc0d088d799490
rr875695 <122020904+rr875695@users.noreply.github.com>
2025-09-25 02:42:44 +0530

    Update api.ts

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,5 +1,5 @@
 // frontend/lib/api.ts
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:5000/api";
+const API_BASE_URL = "https://shikhathekua-1.onrender.com/";
 
 // Get auth token from localStorage
 const getAuthToken = () => {
