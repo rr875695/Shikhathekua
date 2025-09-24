8f0184cf919913e49b74ad09a6fd8ec2c8e38ff3
rr875695 <122020904+rr875695@users.noreply.github.com>
2025-09-25 01:54:04 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // MongoDB Configuration
-  MONGODB_URI: process.env.MONGODB_URI ,
+  MONGODB_URI: mongodb+srv://rr875695:Ravi@2003@cluster0.bregvnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0,
   
   // JWT Secret Key
   JWT_SECRET: process.env.JWT_SECRET || 'thekua_super_secret_key_2024_secure_authentication',
